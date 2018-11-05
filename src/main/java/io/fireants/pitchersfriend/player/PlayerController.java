9d671b36ddc3381d5c2bd89a9501cdabe064f432
Aaron Sweers <aaron.sweers@gmail.com>
2018-11-05 17:18:17 -0600

    fix player endpoint

--- src/main/java/io/fireants/pitchersfriend/player/PlayerController.java
+++ src/main/java/io/fireants/pitchersfriend/player/PlayerController.java
@@ -1,7 +1,6 @@
 package io.fireants.pitchersfriend.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +13,8 @@ public class PlayerController {
 	@Autowired
 	private PlayerService playerService;
 
-	@Cacheable(value = "player", key = "#id")
 	@GetMapping("/{id}")
-	public Player getPlayer(@PathVariable Integer id) {
+	public Player  getPlayer(@PathVariable Integer id) {
 		return playerService.getPlayer(id);
 	}
 }

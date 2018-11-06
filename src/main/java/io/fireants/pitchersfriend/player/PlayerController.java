8a7101a956bb3bb6c9eeee515f141df1c51f3146
Aaron Sweers <aaron.sweers@gmail.com>
2018-11-05 21:12:28 -0600

    returns 404 when id is not found

--- src/main/java/io/fireants/pitchersfriend/player/PlayerController.java
+++ src/main/java/io/fireants/pitchersfriend/player/PlayerController.java
@@ -1,6 +1,8 @@
 package io.fireants.pitchersfriend.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,12 @@ public class PlayerController {
 	private PlayerService playerService;
 
 	@GetMapping("/{id}")
-	public Player  getPlayer(@PathVariable Integer id) {
-		return playerService.getPlayer(id);
+	public ResponseEntity<Player> getPlayer(@PathVariable Integer id) {
+		Player player = playerService.getPlayer(id);
+		if (player == null) {
+			return new ResponseEntity<Player>(HttpStatus.NOT_FOUND);
+	    } else {
+	    		return new ResponseEntity<Player>(player, HttpStatus.OK);
+	    }
 	}
 }
